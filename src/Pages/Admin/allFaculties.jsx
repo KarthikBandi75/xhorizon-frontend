81278fdefe5b01c06e85f78b792bb8c3f235d1a4
BANDI KARTHIK <bandikarthik75@gmail.com>
2025-04-29 15:08:18 +0530

    Update allFaculties.jsx

--- src/Pages/Admin/allFaculties.jsx
+++ src/Pages/Admin/allFaculties.jsx
@@ -127,7 +127,7 @@ const AllFaculties = () => {
                 whileHover={{ scale: 1.03 }}
                 whileTap={{ scale: 0.97 }}
                 onClick={() => deleteFaculty(faculty._id)}
-                className="mt-auto w-full bg-[#EF4444] hover:bg-[#DC2626] text-white py-3 rounded-lg text-sm transition-all cursor-pointer"
+                className="mt-auto w-full bg-[#EF4444] hover:bg-[#DC2626] text-white py-2 rounded-lg text-sm transition-all cursor-pointer"
               >
                 Delete Faculty
               </motion.button>
